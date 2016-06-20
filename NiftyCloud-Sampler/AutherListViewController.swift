8a838680c41f0d4db31d0c5c223bb87e5fc29e4c
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-20 10:06:22 +0900

    [Create] AutherListViewController

---
+++ NiftyCloud-Sampler/AutherListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AutherListViewController.swift
+//  NiftyCloud-Sampler
+//
+//  Created by ShinokiRyosei on 2016/06/20.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class AutherListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
