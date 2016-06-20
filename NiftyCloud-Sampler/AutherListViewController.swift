eba424980be513bba085e263254f600c16d7b2c7
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-20 10:12:40 +0900

    [Add] tableview delegate method

--- NiftyCloud-Sampler/AutherListViewController.swift
+++ NiftyCloud-Sampler/AutherListViewController.swift
@@ -7,17 +7,36 @@
 //
 
 import UIKit
+import NCMB
 
-class AutherListViewController: UIViewController {
+class AutherListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var autherTable: UITableView!
+    
+    var authers: [Authers] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        autherTable.dataSource = self
+        autherTable.delegate = self
+        
+        autherTable.registerNib(UINib(nibName: "AutherCell", bundle: nil), forCellReuseIdentifier: "AutherCell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return authers.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("AutherCell") as! AutherCell
+        
+        return cell
     }
 }
