8f6571f7a33503a126a29176a7c0a458f98cdfe9
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-21 21:49:18 +0900

    [Add] import NCMB

--- NiftyCloud-Sampler/LoginViewController.swift
+++ NiftyCloud-Sampler/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import NCMB
 
 class LoginViewController: UIViewController {
 
@@ -20,16 +21,4 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
