584472438c091956269c6229c83717dfb26e43b5
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-28 18:47:56 +0900

    [Add] Email Confirmation

--- NiftyCloud-Sampler/LoginViewController.swift
+++ NiftyCloud-Sampler/LoginViewController.swift
@@ -31,6 +31,7 @@ class LoginViewController: UIViewController {
     @IBAction func didSelectLogin() {
         guard let mail = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
+        
         self.login(mail, password: password)
     }
     
@@ -40,9 +41,23 @@ class LoginViewController: UIViewController {
                 print(error.localizedDescription)
             }else {
                 if user.isAuthenticated() {
-                    
+                    self.transition()
+                }else {
+                    self.presentAuthAlert()
                 }
             }
         }
+    }
+    
+    func presentAuthAlert() {
+        let alert = UIAlertController(title: "ユーザー認証失敗", message: "メールアドレスの認証を行ってください。", preferredStyle: .Alert)
+        let btn = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(btn)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func transition() {
+        let tabCon = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("tabCon")
+        self.presentViewController(tabCon, animated: true, completion: nil)
     }
 }
