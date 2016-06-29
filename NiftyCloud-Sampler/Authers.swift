3db52f2a4dfa2e0c3109e9423fce4b675571f791
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-30 06:33:57 +0900

    [Add] property in Authers model

--- NiftyCloud-Sampler/Authers.swift
+++ NiftyCloud-Sampler/Authers.swift
@@ -12,6 +12,24 @@ import NCMB
 @objc(Authers)
 class Authers: NCMBObject, NCMBSubclassing {
     
+    var familyName : String {
+        get {
+            return objectForKey("familyName") as! String
+        }
+        set {
+            setObject(newValue, forKey: "familyName")
+        }
+    }
+    
+    var firstName: String {
+        get {
+            return objectForKey("firstName") as! String
+        }set {
+            setObject(newValue, forKey: "firstName")
+        }
+    }
+    
+    
     override init!(className: String!) {
         super.init(className: "Authers")
     }
