004b5dce18d5df1c69cda7be746910f3f2a46250
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-12-29 00:08:44 +0900

    [Update] swift coding style

--- NiftyCloud-Sampler/Cells/BookCell/BookCell.swift
+++ NiftyCloud-Sampler/Cells/BookCell/BookCell.swift
@@ -17,13 +17,5 @@ class BookCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(isSelected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
